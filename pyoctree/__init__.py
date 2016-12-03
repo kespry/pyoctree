49f74c94ee39a44640d70b8ea72e4b715ba910dc
John Laxson <jlaxson@mac.com>
2016-12-03 12:10:00 -0800

    Build cleanup

--- pyoctree/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2015 Michael Hogg
-
-# This file is part of pyoctree - See LICENSE.txt for information on usage and redistribution
-
-# Version
-import version
-__version__ = version.version
